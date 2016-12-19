ee7ab0fa9f78a82364995b280c48f5a220f0883a
LynnWooo <Zion@ZiondeMacBook-Air.local>
2016-12-19 20:14:50 +0800

    observer

--- classes/ScoreBoard.cpp
+++
@@ -1,8 +0,0 @@
-#include"ScoreBoard.h"
-
-void ScoreBoard::OnUpdate(Pinball* Sender, int height){
-	if (_label != NULL){
-		__String* str = __String::createWithFormat("Height: %d", height);
-		_label->setString(str->getCString());
-	}
-}
